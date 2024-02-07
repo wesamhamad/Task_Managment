fe7a5551369eef14b226b9369b5283585b560218
Wesam <74800962+wesamhamad@users.noreply.github.com>
2024-02-07 21:44:27 +0300

    feat: Improve user view determination logic in HomeController

--- laravel-app/app/Http/Controllers/HomeController.php
+++ laravel-app/app/Http/Controllers/HomeController.php
@@ -1,45 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-// use App\Http\Model\User;
-
-
-
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+    /** Show the application view based on user type. **/
     public function index()
     {
         if (Auth::id()) {
             $usertype = Auth::user()->usertype;
 
             if ($usertype == 'user') {
-                return view('home');
+                return view('user.home');
             } else if ($usertype == 'admin') {
                 return view('admin.adminhome');
             }
         } else {
             return redirect()->back();
         }
-
-
     }
 
     public function post()
