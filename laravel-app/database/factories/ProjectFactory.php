37c94f881dc55f2c4ed2c9aa2e3305252861630a
Wesam <74800962+wesamhamad@users.noreply.github.com>
2024-02-16 17:21:31 +0300

    feat: Add Project factory for generating test data

---
+++ laravel-app/database/factories/ProjectFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'created_by' => function () {
+                return \App\Models\User::factory()->create(['usertype' => 'admin'])->id;
+            },
+        ];
+    }
+}
