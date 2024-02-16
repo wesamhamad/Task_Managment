76887d41f59f42a38a8b61b35bf53c07320d7e1b
Wesam <74800962+wesamhamad@users.noreply.github.com>
2024-02-16 17:05:58 +0300

    feat: Add users() relationship method to Tasks model

--- laravel-app/app/Models/Tasks.php
+++ laravel-app/app/Models/Tasks.php
@@ -8,13 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Tasks extends Model
 {
     protected $table = 'user_tasks';
-    protected $fillable = ['name', 'assignTo', 'deadlin', 'status', 'description', 'created_by'];
+    protected $fillable = ['name', 'deadlin', 'status', 'description', 'created_by'];
+
+    public function users()
+    {
+        // The 'task_user' table is used as the pivot table in this relationship.
+        return $this->belongsToMany(User::class, 'task_user', 'task_id', 'user_id');
+    }
 
     // Defines a many-to-many relationship between tasks and projects.
     public function projects()
     {
         // The 'project_task' table is used as the pivot table in this relationship.
-        return $this->belongsToMany(Project::class, 'project_task');
+        return $this->belongsToMany(Project::class, 'project_task', 'task_id', 'project_id');
     }
 
     use HasFactory;
